ef760a565074440a66c1c9457e5692449257eecd
hejiyan <hejiyan@mail.ustc.edu.cn>
2017-08-09 12:37:32 +0800

    add tests

---
+++ tests/test_basic.py
@@ -0,0 +1,11 @@
+from django.test import TestCase, Client
+
+
+class BasicTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_index(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
