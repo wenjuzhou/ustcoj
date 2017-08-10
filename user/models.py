3b36aed5b6ea837a363b74ac262fcdc56d04da62
hejiyan <hejiyan@mail.ustc.edu.cn>
2017-08-11 02:31:20 +0800

    add user.profile.get_avatar_url

--- user/models.py
+++ user/models.py
@@ -16,6 +16,20 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+    def get_avatar_url(self, size=40):
+        """
+        Get user's gravatar url
+        :return:
+        """
+        import hashlib
+        import urllib.parse
+
+        email = self.user.email.encode('utf-8')
+        gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest()
+        gravatar_url += '?' + urllib.parse.urlencode({'s': str(size)})
+
+        return gravatar_url
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
