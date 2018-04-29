4ba45170d641ea009532d1a10cbbd94d25836c26
hejiyan <hejiyan@mail.ustc.edu.cn>
2018-04-29 22:35:33 +0800

    rewrite :(

--- news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<news_id>\d+)/$', views.detail, name='detail')
-]
--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding : utf-8
-"""
-@time   : 09/08/2017 12:17
-@author : Jiyan He <hejiyan@mail.ustc.edu.cn>
-@file   : __init__.py
-
-
-"""
